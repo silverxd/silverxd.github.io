9eec890fe9d830367aeaa5b435cda09f093c7278
rkilks <rkilks@taltech.ee>
2023-11-18 18:55:19 +0000

    Resolve "Debug clickeri upgrades andmebaasiga siduda"

--- src/app/clicker/clicker.service.ts
+++ src/app/clicker/clicker.service.ts
@@ -11,9 +11,84 @@ import {from, map, Observable, of, switchMap, throttleTime, timer} from "rxjs";
 export class ClickerService {
   user: User | null;
   authState$: Observable<User | null>;
-
+  upgradesUpdated: any;
+  autosaveDate = new Date()
+  upgradesDefault: {
+    name: string,
+    cost: number,
+    debuxPerSec?: number,
+    debuxPerClick?: number,
+    affordable: boolean,
+    purchased: number,
+    description: string
+  }[] = [
+    {
+      name: 'Multithreading', cost: 10, debuxPerSec: 1, affordable: false, purchased: 0,
+      description: 'Increase processing power by utilizing multiple threads.'
+    },
+    {
+      name: 'Optimization Algorithms', cost: 100, debuxPerSec: 10, affordable: false, purchased: 0,
+      description: 'Enhance efficiency through advanced code optimization techniques.'
+    },
+    {
+      name: 'Parallel Processing', cost: 1000, debuxPerSec: 100, affordable: false, purchased: 0,
+      description: 'Enable simultaneous execution for faster processing.'
+    },
+    {
+      name: 'Overclocking', cost: 10000, debuxPerSec: 1000, affordable: false, purchased: 0,
+      description: 'Push the CPU to run faster, boosting processing power.'
+    },
+    {
+      name: 'Hardware Upgrades', cost: 100000, debuxPerSec: 10000, affordable: false, purchased: 0,
+      description: 'Install better processors or RAM to increase overall performance.'
+    },
+    {
+      name: 'Cooling Systems', cost: 1000000, debuxPerSec: 100000, affordable: false, purchased: 0,
+      description: 'Optimize cooling mechanisms to sustain higher processing speeds.'
+    },
+    {
+      name: 'Syntax Highlighting', cost: 1, debuxPerClick: 1, affordable: false, purchased: 0,
+      description: 'Boost click effectiveness with highlighted code segments.'
+    },
+    {
+      name: 'Code Formatting', cost: 10, debuxPerClick: 10, affordable: false, purchased: 0,
+      description: 'Improve the appearance of code to enhance click impact.'
+    },
+    {
+      name: 'Auto-Completion', cost: 100, debuxPerClick: 100, affordable: false, purchased: 0,
+      description: 'Increase code suggestion accuracy to amplify your clicks.'
+    },
+    {
+      name: 'GUI Enhancements', cost: 1000, debuxPerClick: 1000, affordable: false, purchased: 0,
+      description: 'Upgrade the interface for a more responsive and impactful clicking experience.'
+    },
+    {
+      name: 'Mouse Acceleration', cost: 10000, debuxPerClick: 10000, affordable: false, purchased: 0,
+      description: 'Improve click effectiveness by enhancing mouse sensitivity.'
+    },
+    {
+      name: 'ChatGPT Evolution', cost: 1000, affordable: false, purchased: 0,
+      description: 'Upgrade ChatGPT to a more advanced version, increasing its problem-solving capabilities and assistance in debugging.'
+    },
+    {
+      name: 'Automated Testing', cost: 10000, affordable: false, purchased: 0,
+      description: 'Develop a robust automated testing system that quickly identifies and reports bugs, making debugging more efficient.'
+    },
+    {
+      name: 'Code Refactoring AI', cost: 100000, affordable: false, purchased: 0,
+      description: 'Integrate AI-driven code refactoring to automatically improve code structure and readability during the debugging process.'
+    },
+    {
+      name: 'Codebase Analysis', cost: 1000000, affordable: false, purchased: 0,
+      description: 'Unlock in-depth analysis tools that provide insights into code quality, performance, and potential issues, aiding in targeted debugging efforts.'
+    },
+    {
+      name: 'Tabnine', cost: 10000000, affordable: false, purchased: 0,
+      description: 'Upgrade Tabnine\'s capabilities, allowing it to suggest code snippets and completions at an accelerated rate, improving coding speed and accuracy.'
+    },
+  ];
   private debuxValue: number = 0;
-  private readonly saveInterval = 60 * 1000; // Autosave every 60 seconds
+  private readonly saveInterval = 60; // Autosave every 60 seconds
   private purchasedUpgradesPerSec: { name: string, debuxPerSec: number }[] = [];
   private purchasedUpgradesPerClick: { name: string, debuxPerClick: number }[] = [];
 
@@ -31,20 +106,41 @@ export class ClickerService {
   }
 
   buyUpgrade(upgradeName: string, debuxPerSec: number, debuxPerClick: number): void {
-    if (typeof debuxPerSec !== 'undefined') {
-      this.purchasedUpgradesPerSec.push({ name: upgradeName, debuxPerSec });
-    }
-    else if (typeof debuxPerClick !== 'undefined') {
-      this.purchasedUpgradesPerClick.push({ name: upgradeName, debuxPerClick });
+    if (typeof debuxPerSec !== 'undefined' && debuxPerSec !== 0) {
+      console.log('DebuxPerSec purchased')
+      this.purchasedUpgradesPerSec.push({name: upgradeName, debuxPerSec});
+    } else if (typeof debuxPerClick !== 'undefined' && debuxPerClick !== 0) {
+      console.log('DebuxPerClick purchased')
+      this.purchasedUpgradesPerClick.push({name: upgradeName, debuxPerClick});
     }
   }
 
   calculateTotalDebuxPerClick(): number {
-    return this.purchasedUpgradesPerClick.reduce((total, upgrade) => total + upgrade.debuxPerClick, 1);
+    return this.purchasedUpgradesPerClick.reduce((total, upgrade) =>
+      total + upgrade.debuxPerClick, 1
+    );
   }
 
   calculateTotalDebuxPerSec(): number {
-    return this.purchasedUpgradesPerSec.reduce((total, upgrade) => total + upgrade.debuxPerSec, 0);
+    return this.purchasedUpgradesPerSec.reduce((total, upgrade) =>
+      total + upgrade.debuxPerSec, 0
+    );
+  }
+
+  firstCalc(upgrades: any) {
+    let totalDebuxPerClick = 0;
+    let totalDebuxPerSec = 0;
+    for (const key in upgrades) {
+      const value = upgrades[key]
+      if (value.debuxPerSec != null) {
+        totalDebuxPerSec = value.debuxPerSec * value.purchased;
+      }
+      if (value.debuxPerClick != null) {
+        totalDebuxPerClick = value.debuxPerClick * value.purchased
+      }
+      // console.log(value.name, totalDebuxPerSec, totalDebuxPerClick)
+      this.buyUpgrade(value.name, totalDebuxPerSec, totalDebuxPerClick)
+    }
   }
 
   addDebux(debux: number): void {
@@ -52,9 +148,13 @@ export class ClickerService {
     this.debuxValue = debux; // To show the value change right away to user.
   }
 
+  addUpgrades(upgrades: any) {
+    this.upgradesUpdated = upgrades
+  }
+
   private startAutosave(): void {
     // Use throttleTime to limit the frequency of save operations
-    timer(0, this.saveInterval) //  regular intervals (this.saveInterval).
+    timer(0, this.saveInterval * 1000) //  regular intervals (this.saveInterval).
       .pipe(
         switchMap(() => this.autosave()), // The switchMap operator is used to switch to the result of the autosave method.
         throttleTime(this.saveInterval) // The throttleTime operator limits the frequency of autosave operations to prevent excessive calls.
@@ -62,15 +162,25 @@ export class ClickerService {
       .subscribe();
   }
 
-  private autosave(): Observable<void> {
+  autosave(): Observable<void> {
     // If user is authenticated and there are changes to the value.
+    if (this.user && this.debuxValue != 0) {
+      // Creates a dict or batch to send the data in one write.
+      const updateData: any = {
+        Debux: this.debuxValue,
+      };
 
-    if (this.user && this.debuxValue > 0) {
-      // Send the totalDebux value to the Database
-      return from(this.db.doc(`User/${this.user.uid}`).update({Debux: this.debuxValue})).pipe(
+      if (this.upgradesUpdated) {
+        updateData.upgrades = this.upgradesUpdated
+      }
+
+      this.autosaveDate = new Date();
+
+      return from(this.db.doc(`User/${this.user?.uid}`).update(updateData)).pipe(
         map(() => {
           // Clear the queue after a successful write
           this.debuxValue = 0;
+          console.log('cleared')
         })
       );
     } else {
@@ -91,6 +201,28 @@ export class ClickerService {
           return of(undefined); // Return an observable that completes immediately with undefined
         }
       })
-  );
+    );
+  }
+
+  sendUpgrades(upgradesIn: any) {
+    const userDocRef = this.db.collection('User').doc(this.user?.uid);
+    userDocRef.set({upgrades: upgradesIn}, {merge: true});
+
+  }
+
+  getUpgrades(): Observable<any[]> {
+    return this.authState$.pipe(
+      switchMap((user) => {
+        if (user) {
+          const userDocRef = this.db.collection('User').doc(user.uid);
+          return userDocRef.valueChanges().pipe(
+            map((userUID: any) => userUID ? userUID.upgrades || this.upgradesDefault : [])
+          );
+        } else {
+          console.warn('User not authenticated.');
+          return of([]); // Return an observable that completes immediately with an empty array
+        }
+      })
+    );
   }
 }
