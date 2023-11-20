0d17ad3c5656c1d25b719202a66ba5d107e4ea11
Rasmus <rraasu@taltech.ee>
2023-11-20 11:35:19 +0200

    Natuke vajab veel noktitsemist.

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -10,9 +10,37 @@ import { OverlayService } from '../overlay.service';
   styleUrl: './settings.component.css'
 })
 export class SettingsComponent {
+  selectedBox: string = 'Default'; // Default selected box
+  content: any = {
+    'Default': {
+      title: 'Settings page',
+      description: 'Customize everything to your liking!'
+    },
+    'Appearence': {
+      title: 'Appearence Settings',
+      description: 'Customize the visual appearence of your application.'
+    },
+    'Privacy': {
+      title: 'Privacy Settings',
+      description: 'Manage your privacy preferences and settings.'
+    },
+    'Status': {
+      title: 'Status Settings',
+      description: 'Configure your status and online/offline preferences.'
+    }
+  };
+
   constructor(private overlayService: OverlayService) {}
 
   closeOverlay() {
     this.overlayService.closeOverlay();
+  }
+
+  changeContent(box: string) {
+    this.selectedBox = box;
+  }
+
+  isBoxSelected(box: string): boolean {
+    return this.selectedBox === box;
   }
 }
