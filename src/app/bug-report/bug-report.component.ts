ed3c963dcaf03e577bc5b5b414e921e408848288
Rasmus <rraasu@taltech.ee>
2023-11-21 19:16:08 +0200

    Dropdown valiku osa on tehtud

--- src/app/bug-report/bug-report.component.ts
+++ src/app/bug-report/bug-report.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild, TemplateRef, ViewContainerRef } from '@angular/core';
+import { Component, ElementRef, ViewChild, TemplateRef, ViewContainerRef, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OverlayService } from '../overlay.service';
 import { Overlay, OverlayRef, ConnectedPosition } from '@angular/cdk/overlay';
@@ -11,42 +11,66 @@ import { TemplatePortal } from '@angular/cdk/portal';
   templateUrl: './bug-report.component.html',
   styleUrl: './bug-report.component.css'
 })
-export class BugReportComponent {
+export class BugReportComponent implements AfterViewInit {
   @ViewChild('dropdownTemplate', { static: true }) dropdownTemplate!: TemplateRef<any>; // Reference to your dropdown content template
-  private overlayRef: OverlayRef;
+  private overlayRef!: OverlayRef;
+  problems: string[] = ['Transactions', 'Messages', 'Account', 'Credit', 'Posts', 'Other']
+  problem_with: string = "Choose here..."
+  public dropdownIsOpen: boolean = false
 
   constructor(
     private overlay: Overlay,
     private overlayService: OverlayService,
     private viewContainerRef: ViewContainerRef,
     private elementRef: ElementRef,
-  ) {
-    const positionStrategy = this.overlay.position()
-      .flexibleConnectedTo(this.elementRef)
-      .withPositions([
-        { originX: 'start', originY: 'bottom', overlayX: 'start', overlayY: 'top' }
-      ]);
+  ) {}
 
-    this.overlayRef = this.overlay.create({
-      positionStrategy,
-      hasBackdrop: true,
-      backdropClass: 'dropdown-backdrop',
-      panelClass: "mat-elevation-z8",
-    });
-
-    this.overlayRef.backdropClick().subscribe(() => this.toggleDropdown());
+  ngAfterViewInit() {
+    this.initializeOverlay();
   }
+
+  private initializeOverlay() {
+    const dropdownElement = this.elementRef.nativeElement.querySelector('.dropdown');
+    
+    if (dropdownElement) {
+      const positionStrategy = this.overlay.position()
+        .flexibleConnectedTo(dropdownElement)
+        .withPositions([
+          { originX: 'start', originY: 'bottom', overlayX: 'start', overlayY: 'top'}
+        ]);
+
+      this.overlayRef = this.overlay.create({
+        positionStrategy,
+        hasBackdrop: true,
+        backdropClass: 'dropdown-backdrop',
+      });
+
+      this.overlayRef.backdropClick().subscribe(() => this.toggleDropdown());
+    }
+  }
+
 
   toggleDropdown() {
     if (this.overlayRef.hasAttached()) {
       this.overlayRef.detach();
+      this.dropdownIsOpen = false;
     } else {
       const portal = new TemplatePortal(this.dropdownTemplate, this.viewContainerRef);
       this.overlayRef.attach(portal);
+      this.dropdownIsOpen = true;
     }
   }
   
   closeOverlay() {
     this.overlayService.closeOverlay();
+  }
+
+  changeDropdownDisplayHover(name: string) {
+    this.problem_with = name
+  }
+
+  changeDropdownDisplayClick(name: string) {
+    this.problem_with = name
+    this.toggleDropdown()
   }
 }
