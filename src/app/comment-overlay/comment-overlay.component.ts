ed194a812cabd5a9701fc2f0a0fb1df2525ce967
Silver <sislin@taltech.ee>
2023-11-29 22:32:53 +0200

    stiil korras

--- src/app/comment-overlay/comment-overlay.component.ts
+++ src/app/comment-overlay/comment-overlay.component.ts
@@ -15,7 +15,8 @@ export class CommentOverlayComponent {
     private overlayService: OverlayService,
   ) {}
   // comments: any;
-  comments = [{'comment': 'See kommentaar ei ole (veel) laetud andmebaasist! :(', 'profile_pic': "https://media.discordapp.net/attachments/1149290454819553420/1164864763105390592/b61f156cac360ab208b71ce5102ee9c5_400x400.png", "display_name": "Silver", "timestamp": "29/11/2023, 5:46 AM", }]
+  comments = [{'comment': 'See kommentaar ei ole (veel) laetud andmebaasist! :(', 'profile_pic': "https://media.discordapp.net/attachments/1149290454819553420/1164864763105390592/b61f156cac360ab208b71ce5102ee9c5_400x400.png", "display_name": "Silver", "timestamp": "29/11/2023, 5:46 AM", },
+  {'comment': 'TODO: laadida kommentaarid andmebaasist?', 'profile_pic': "https://media.discordapp.net/attachments/1149290454819553420/1164864763105390592/b61f156cac360ab208b71ce5102ee9c5_400x400.png", "display_name": "Silver", "timestamp": "29/11/2023, 5:47 AM", }]
   closeOverlay() {
     this.overlayService.closeOverlay();
   }
