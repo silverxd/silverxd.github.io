42555422b312033c1047abd3e521d666fc5ebccf
Silver <sislin@taltech.ee>
2023-12-06 23:31:14 +0200

    ajutine lahendus

--- src/app/comment-overlay/comment-overlay.component.ts
+++ src/app/comment-overlay/comment-overlay.component.ts
@@ -14,13 +14,10 @@ export class CommentOverlayComponent {
   constructor(
     private overlayService: OverlayService,
   ) {}
-  comments: any;
+  // comments: any;
 
-  // comments = [{'comment': 'See kommentaar ei ole (veel) laetud andmebaasist! :(', 'profile_pic': "https://media.discordapp.net/attachments/1149290454819553420/1164864763105390592/b61f156cac360ab208b71ce5102ee9c5_400x400.png", "display_name": "Silver", "timestamp": "29/11/2023, 5:46 AM", },
-  // {'comment': 'TODO: laadida kommentaarid andmebaasist?', 'profile_pic': "https://media.discordapp.net/attachments/1149290454819553420/1164864763105390592/b61f156cac360ab208b71ce5102ee9c5_400x400.png", "display_name": "Silver", "timestamp": "29/11/2023, 5:47 AM", }]
-  openOverlay() {
-    this.overlayService.openOverlay(CommentOverlayComponent);
-  }
+  comments = [{'comment': 'See kommentaar ei ole (veel) laetud andmebaasist! :(', 'profile_pic': "https://media.discordapp.net/attachments/1149290454819553420/1164864763105390592/b61f156cac360ab208b71ce5102ee9c5_400x400.png", "display_name": "Silver", "timestamp": "29/11/2023, 5:46 AM", },
+  {'comment': 'Also, Rasmus lasi kogu postituste baasi eelviimasel nädalal õhku :D', 'profile_pic': "https://media.discordapp.net/attachments/1149290454819553420/1164864763105390592/b61f156cac360ab208b71ce5102ee9c5_400x400.png", "display_name": "Silver", "timestamp": "29/11/2023, 5:47 AM", }]
   
   closeOverlay() {
     this.overlayService.closeOverlay();
